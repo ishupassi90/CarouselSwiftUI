996d8202d4c1560648e26f4f46dbeaa9e86d295b
Ishu Passi <ishu@vaultspay.ae>
2024-08-28 13:08:01 +0530

    Carousel

---
+++ CarouselSwiftUI/CarouselSwiftUI/CarouselSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSwiftUIApp.swift
+//  CarouselSwiftUI
+//
+//  Created by Ishu Passi on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
